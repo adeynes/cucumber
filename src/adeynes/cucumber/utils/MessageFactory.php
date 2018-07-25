fc17d5f2f7b6c1c426035aed027ac024617f8421
adeynes <alexisdeynes@outlook.com>
2018-07-25 14:30:10 +0200

    MessageFactory returns early if there are no tags (prevents undefined offset)

--- src/adeynes/cucumber/utils/MessageFactory.php
+++ src/adeynes/cucumber/utils/MessageFactory.php
@@ -22,6 +22,9 @@ final class MessageFactory
         preg_match_all('/%(.*?)%/', $template,$tags);
         // Make sure we only have unique values; str_replace replaces everything anyways
         $tags = array_unique($tags, SORT_REGULAR);
+
+        if (empty($tags)) return $template;
+
         // Given %tag%, $tags[1] will be "tag" while $tags[0] will be "%tag%"
         foreach ($tags[1] as $key => $tag)
             $template = str_replace($tags[0][$key], $data[$tag], $template);
